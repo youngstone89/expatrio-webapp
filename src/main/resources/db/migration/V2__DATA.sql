153560fc3ff5550ee189a6e1c5961bbcc0e961da
youngstone <youngstone89@gmail.com>
2020-01-23 23:23:00 +0900

    init admin user with bcrypted password

--- src/main/resources/db/migration/V2__DATA.sql
+++ src/main/resources/db/migration/V2__DATA.sql
@@ -1,2 +1,7 @@
 INSERT INTO roles(name) VALUES('ROLE_ADMIN');
 INSERT INTO roles(name) VALUES('ROLE_CUSTOMER');
+INSERT INTO users (email,password,username)
+VALUES ('admin@expatrio.com','$2a$10$aJEScajS63z/K.XLhFOuIuLW.z.ftK5DbwpUnmhdcUSnT6AABVr92','admin');
+
+INSERT INTO user_roles (user_id,role_id)
+VALUES (1,1);
